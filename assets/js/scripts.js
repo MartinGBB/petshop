c87b9b551620cd9587e50b3828fa5e86e24eca0a
MartinGBB <martingbrazon@gmail.com>
2024-06-17 23:13:54 -0300

    feat: implementa produtos e servicos com adicao automatica de paginas

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,70 +1,89 @@
 document.addEventListener("DOMContentLoaded", function () {
-   // Verificar se o usuário está logado
-   const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
- 
-   // Função para carregar o conteúdo
-   function loadContent(url, containerId, callback) {
-     fetch(url)
-       .then(response => response.text())
-       .then(data => {
-         document.getElementById(containerId).innerHTML = data;
-         if (callback) {
-           callback();
-         }
-       });
-   }
- 
-   // Função para carregar o cabeçalho e rodapé
-   function loadPartials() {
-     fetch("partials/header.html")
-       .then(response => response.text())
-       .then(data => {
-         document.getElementById("header").innerHTML = data;
-         setupNavigation();
-       });
- 
-     fetch("partials/footer.html")
-       .then(response => response.text())
-       .then(data => {
-         document.getElementById("footer").innerHTML = data;
-       });
- 
-     fetch("partials/menu.html")
-       .then(response => response.text())
-       .then(data => {
-         document.getElementById("menu").innerHTML = data;
-       });
-   }
- 
-   // Função para configurar o formulário de login
-   function setupLoginForm() {
-     const loginForm = document.getElementById('login-form');
-     if (loginForm) {
-       loginForm.addEventListener('submit', function (event) {
-         event.preventDefault();
-         // Simulação de login - em um cenário real, você faria uma verificação de credenciais
-         localStorage.setItem('isLoggedIn', 'true');
-         loadContent("pages/home.html", "content", loadPartials);
-       });
-     }
-   }
- 
-   // Função para configurar a navegação
-   function setupNavigation() {
-     document.querySelectorAll('a.nav-link').forEach(link => {
-       link.addEventListener('click', function (event) {
-         event.preventDefault();
-         const targetPage = this.getAttribute('href');
-         loadContent(`pages/${targetPage}`, "content");
-       });
-     });
-   }
- 
-   // Carregar a página correta com base no estado de login
-   if (isLoggedIn) {
-     loadContent("pages/home.html", "content", loadPartials);
-   } else {
-     loadContent("pages/login.html", "content", setupLoginForm);
-   }
- });
- 
+  localStorage.setItem('isLoggedIn', true)
+  const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
+
+  function loadContent(url, containerId, callback) {
+    fetch(url)
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById(containerId).innerHTML = data;
+        if (callback) {
+          callback();
+        }
+      });
+  }
+
+  function loadPartials() {
+    fetch("/partials/header.html")
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById("header").innerHTML = data;
+        setupNavigation();
+      });
+
+    fetch("/partials/footer.html")
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById("footer").innerHTML = data;
+      });
+
+    fetch("/partials/menu.html")
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById("menu").innerHTML = data;
+        setupNavigation();
+      });
+  }
+
+  function setupLoginForm() {
+    const loginForm = document.getElementById('login-form');
+    if (loginForm) {
+      loginForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        localStorage.setItem('isLoggedIn', 'true');
+        loadContent("pages/home.html", "content", loadPartials);
+      });
+    }
+  }
+
+  function setupNavigation() {
+    document.querySelectorAll('.redirect').forEach(link => {
+      link.addEventListener('click', function (event) {
+        event.preventDefault();
+        const targetPage = this.getAttribute('data-redirect-to');
+        loadContent(`${targetPage}`, "content");
+      });
+    });
+
+    // Adiciona manipuladores de eventos para links de produtos
+    document.querySelectorAll('.product-link').forEach(link => {
+      link.addEventListener('click', function (event) {
+        event.preventDefault();
+        const productId = this.getAttribute('data-product-id');
+        loadProductPage(productId);
+      });
+    });
+  }
+
+  function loadPageContent() {
+    const bodyClass = document.body.classList[0];
+    console.log(bodyClass)
+    switch (bodyClass) {
+      case 'products-page':
+        loadContent("/pages/products/product.html", "main-content", loadPartials);
+        break;
+      case 'services-page':
+        loadContent("/pages/services/services.html", "main-content", loadPartials);
+        break;
+      default:
+        loadContent("pages/home.html", "content", loadPartials);
+        break;
+    }
+  }
+
+  if (isLoggedIn) {
+    loadPageContent();
+  } else {
+    loadContent("pages/login.html", "content", setupLoginForm);
+  }
+});
